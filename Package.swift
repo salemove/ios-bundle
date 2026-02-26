c554cc4f7bf02229a555c21f3ed74927848a7284
BitriseBot <bitrise@glia.com>
2026-02-26 11:35:48 +0000

    Glia Core SDK Release 2.6.4
    Glia Core SDK Release 2.6.4

--- Package.swift
+++ Package.swift
@@ -34,13 +34,13 @@ let package = Package(
         ),
         .binaryTarget(
             name: "GliaCoreSDK",
-            url: "https://github.com/salemove/ios-bundle/releases/download/2.6.3/GliaCoreSDK.xcframework.zip",
-            checksum: "54d77e2c7a5f9790433138c30a67fa5540f24a42e0ed9aaf21ea6303106346b3"
+            url: "https://github.com/salemove/ios-bundle/releases/download/2.6.4/GliaCoreSDK.xcframework.zip",
+            checksum: "fbf2de1d8d42be06747681c54cb11adca7d0289697782bd868ac020b7a540db5"
         ),
         .binaryTarget(
             name: "GliaOpenTelemetry",
-            url: "https://github.com/salemove/ios-telemetry-bundle/releases/download/1.0.8/GliaOpenTelemetry.xcframework.zip",
-            checksum: "9dd9e68d8312c601a69e73f46adf0132226168baa0e3f21415af47835c4d139d"
+            url: "https://github.com/salemove/ios-telemetry-bundle/releases/download/1.0.9/GliaOpenTelemetry.xcframework.zip",
+            checksum: "dfbc1ff646926ead6690e02c1d69a7a5243f8684da112258025ff1b09013900a"
         ),
         .binaryTarget(
             name: "PhoenixChannelsClient",
